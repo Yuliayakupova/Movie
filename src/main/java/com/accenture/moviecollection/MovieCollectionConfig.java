7820abc52f12e41b63c477b67a94e8e4916748cc
yuliayakupova@gmail.com <BarsukTvojDrug368>
2022-03-15 22:15:51 +0200

    commit -m "small fix"

--- src/main/java/com/accenture/moviecollection/MovieCollectionConfig.java
+++ src/main/java/com/accenture/moviecollection/MovieCollectionConfig.java
@@ -18,21 +18,16 @@ public class MovieCollectionConfig {
 
     @Bean
     public JDBC4Connection getDatabaseConnection(){
-
-        final String url = String.format("jdbc:sqlite:%s",
-                "imdb.db");
-
-        Class.forName("org.sqlite.JDBC");
-
-        try(final JDBC4Connection connection = (JDBC4Connection)  DriverManager.getConnection(url)) {
-
+        try {
+            final String url = String.format("jdbc:sqlite:%s",
+                    "imdb.db");
+            Class.forName("org.sqlite.JDBC");
+            final JDBC4Connection connection = (JDBC4Connection)  DriverManager.getConnection(url);
             connection.setAutoCommit(true);
-
             return connection;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return null;
     }
 }
